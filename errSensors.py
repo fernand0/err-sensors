948ebea6e440e17f190e55a008b2b53203e2dde9
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2021-01-29 13:21:50 +0100

    Cleaning unused code

--- errSensors.py
+++ errSensors.py
@@ -6,22 +6,6 @@ class errSensors(BotPlugin):
     Show the results of reading sensors attached to my Raspberry Pi.
     """
 
-    #def activate(self):
-    #    """
-    #    Triggers on plugin activation
-
-    #    You should delete it if you're not using it to override any default behaviour
-    #    """
-    #    super(Err-sensors, self).activate()
-
-    #def deactivate(self):
-    #    """
-    #    Triggers on plugin deactivation
-
-    #    You should delete it if you're not using it to override any default behaviour
-    #    """
-    #    super(Err-sensors, self).deactivate()
-
     @botcmd(split_args_with=None) 
     def temp(self, message, args): 
         """A command which returns temperature measurement""" 
@@ -65,72 +49,3 @@ class errSensors(BotPlugin):
 
         return res
 
-    #def get_configuration_template(self):
-    #    """
-    #    Defines the configuration structure this plugin supports
-
-    #    You should delete it if your plugin doesn't use any configuration like this
-    #    """
-    #    return {'EXAMPLE_KEY_1': "Example value",
-    #            'EXAMPLE_KEY_2': ["Example", "Value"]
-    #           }
-
-    #def check_configuration(self, configuration):
-    #    """
-    #    Triggers when the configuration is checked, shortly before activation
-
-    #    Raise a errbot.ValidationException in case of an error
-
-    #    You should delete it if you're not using it to override any default behaviour
-    #    """
-    #    super(Err-sensors, self).check_configuration(configuration)
-
-    #def callback_connect(self):
-    #    """
-    #    Triggers when bot is connected
-
-    #    You should delete it if you're not using it to override any default behaviour
-    #    """
-    #    pass
-
-    #def callback_message(self, message):
-    #    """
-    #    Triggered for every received message that isn't coming from the bot itself
-
-    #    You should delete it if you're not using it to override any default behaviour
-    #    """
-    #    pass
-
-    #def callback_botmessage(self, message):
-    #    """
-    #    Triggered for every message that comes from the bot itself
-
-    #    You should delete it if you're not using it to override any default behaviour
-    #    """
-    #    pass
-
-    #@webhook
-    #def example_webhook(self, incoming_request):
-    #    """A webhook which simply returns 'Example'"""
-    #    return "Example"
-
-    ## Passing split_args_with=None will cause arguments to be split on any kind
-    ## of whitespace, just like Python's split() does
-    #@botcmd(split_args_with=None)
-    #def example(self, message, args):
-    #    """A command which simply returns 'Example'"""
-    #    return "Example"
-
-    #@arg_botcmd('name', type=str)
-    #@arg_botcmd('--favorite-number', type=int, unpack_args=False)
-    #def hello(self, message, args):
-    #    """
-    #    A command which says hello to someone.
-
-    #    If you include --favorite-number, it will also tell you their
-    #    favorite number.
-    #    """
-    #    if args.favorite_number is None:
-    #        return f'Hello {args.name}.'
-    #    else:
-    #        return f'Hello {args.name}, I hear your favorite number is {args.favorite_number}.'
